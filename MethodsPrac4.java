5db028550adeb6118c23ac5ef45042f08e7f8e37
franyatta <95552724+franyatta@users.noreply.github.com>
2023-10-14 17:17:25 -0400

    Update MethodsPrac4.java

--- MethodsPrac4.java
+++ MethodsPrac4.java
@@ -1,6 +1,5 @@
 // this method shows how void is used
 // a call to a void method MUST be a statement
-// do not use return
 public class MethodsPrac4 {
 
 	public static void main(String[] args) {
@@ -8,16 +7,20 @@ public class MethodsPrac4 {
 		printGrade(78.5);
 	}
 	public static void printGrade(double score) {
-		if (score >= 90.0)
+		if (score <= 0 || score >= 100) {
+			System.out.print("Invalid score");
+			return; // only use return in void if you want to go back to main method
+		} else if (score >= 90.0) {
 			System.out.print("A");
-		else if (score >= 80.0)
+		} else if (score >= 80.0) {
 			System.out.print("B");
-		else if (score >= 70.0)
+		} else if (score >= 70.0) {
 			System.out.print("C");
-		else if (score >= 60.0)
+		} else if (score >= 60.0) {
 			System.out.print("D");
-		else
+		} else {
 			System.out.print("F");
+		}
 	}
 }
 // franyatta
